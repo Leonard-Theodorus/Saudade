eadb70865022222d6b4c732c31e2932cdfa4b0c8
Leonard Theodorus <aoesotfs@gmail.com>
2024-01-27 18:41:43 +0700

    Initial Commit

---
+++ DanSe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DanSe
+//
+//  Created by Alonica🐦‍⬛🐺 on 27/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DanSe/DanSeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanSeApp.swift
+//  DanSe
+//
+//  Created by Alonica🐦‍⬛🐺 on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DanSeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
